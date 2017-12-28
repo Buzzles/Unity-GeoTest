a245fd2d623d0d3b9d047287140133953db103df
Ian B <ibuswell@gmail.com>
2017-12-28 00:45:20 +0000

    Adding initial scene change script and apply it to Menu Battlescape button

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    public string Destination { get; set; }
+
+	// Use this for initialization
+    void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+    void Update ()
+    {
+	}
+
+    public void SwapScene()
+    {
+        SceneManager.LoadScene(Destination);
+    }
+}
